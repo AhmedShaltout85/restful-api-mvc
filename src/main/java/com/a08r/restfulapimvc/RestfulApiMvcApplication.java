4a798d68bec285d0a8bfef26f73fb526c7a75ee2
AhmedShaltout85 <eng.abozaher@gmail.com>
2024-01-02 09:37:54 +0200

    initial project

---
+++ src/main/java/com/a08r/restfulapimvc/RestfulApiMvcApplication.java
@@ -0,0 +1,13 @@
+package com.a08r.restfulapimvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiMvcApplication.class, args);
+	}
+
+}
